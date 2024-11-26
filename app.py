df222f630927cc39668392f7e6e1a800118c3591
Santhosh Ailneni <109569208+Santhosh-RP@users.noreply.github.com>
2024-11-26 19:43:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,5 +46,5 @@ def predict():
         logging.error(f"Prediction error: {e}")
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8000)
+#if __name__ == '__main__':
+   # app.run(host='0.0.0.0',port=8000)
